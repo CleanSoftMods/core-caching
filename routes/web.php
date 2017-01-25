3e2dd98c31905832473b3af29fcb312391530144
Tedozi Manson <duyphan.developer@gmail.com>
2017-01-25 20:36:18 +0700

    Upgrade to Laravel 5.4

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
-<?php use Illuminate\Routing\Router;
-
-/**
- *
- * @var Router $router
- *
- */
+<?php
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
 
 $adminRoute = config('webed.admin_route');
 
@@ -13,7 +9,7 @@ $moduleRoute = 'caching';
 /**
  * Admin routes
  */
-$router->group(['prefix' => $adminRoute . '/' . $moduleRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
+Route::group(['prefix' => $adminRoute . '/' . $moduleRoute], function (Router $router) use ($adminRoute, $moduleRoute) {
     $router->get('', 'CachingController@getIndex')
         ->name('admin::webed-caching.index.get')
         ->middleware('has-permission:view-caching');
