59ddbcf53cdccf9b5ea3f5ec056624d1c174a9a9
Tedozi Manson <duyphan.developer@gmail.com>
2017-04-13 01:22:03 +0700

    Refactoring

--- src/Providers/InstallModuleServiceProvider.php
+++ src/Providers/InstallModuleServiceProvider.php
@@ -28,7 +28,7 @@ class InstallModuleServiceProvider extends ServiceProvider
 
     }
 
-    private function booted()
+    protected function booted()
     {
         acl_permission()
             ->registerPermission('View cache management page', 'view-cache', $this->module)
