1770f5ba2b5852cc4656bbd82303dcd056f77b3a
Tedozi Manson <duyphan.developer@gmail.com>
2017-04-25 19:23:19 +0700

    Add description for caching commands

--- resources/lang/en/base.php
+++ resources/lang/en/base.php
@@ -7,12 +7,30 @@ return [
     'cache_management' => 'Cache management',
     'cache_commands' => 'Commands',
     'commands' => [
-        'clear_cms_cache' => 'Clear all CMS cache',
-        'refresh_compiled_views' => 'Refresh compiled views',
-        'create_config_cache' => 'Create config cache',
-        'clear_config_cache' => 'Clear config cache',
-        'optimize_class_loader' => 'Optimize class loader',
-        'clear_optimized_class_loader' => 'Clear optimized class loader',
+        'clear_cms_cache' => [
+            'title' => 'Clear all CMS cache',
+            'description' => 'Clear CMS caching: database caching, static blocks... Run this command when you don\'t see the changes after update data.',
+        ],
+        'refresh_compiled_views' => [
+            'title' => 'Refresh compiled views',
+            'description' => 'Clear compiled views to make views up to date.',
+        ],
+        'create_config_cache' => [
+            'title' => 'Create config cache',
+            'description' => 'To give your application a speed boost, you should cache all of your configuration files into a single file. This will combine all of the configuration options for your application into a single file which will be loaded quickly by the framework.',
+        ],
+        'clear_config_cache' => [
+            'title' => 'Clear config cache',
+            'description' => 'You might need to refresh the config caching when you change something on production environment.',
+        ],
+        'optimize_class_loader' => [
+            'title' => 'Optimize class loader',
+            'description' => 'You want your application to run as quickly as possible. ? We will optimize the autoloaded class.',
+        ],
+        'clear_optimized_class_loader' => [
+            'title' => 'Clear optimized class loader',
+            'description' => 'This might makes your app run slower.',
+        ],
     ],
     'messages' => [
         'cache_cleaned' => 'Cache cleaned',
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -23,70 +23,98 @@
                     </h3>
                 </div>
                 <div class="box-body">
-                    <div class="form-group">
-                        <a href="{{ route('admin::webed-caching.clear-cms-cache.get') }}"
-                           data-toggle="confirmation"
-                           data-placement="right"
-                           title="{{ trans('webed-core::messages.are_you_sure') }}"
-                           class="btn btn-danger">
-                            {{ trans('webed-caching::base.commands.clear_cms_cache') }}
-                        </a>
-                        <a href="{{ route('admin::webed-caching.refresh-compiled-views.get') }}"
-                           data-toggle="confirmation"
-                           data-placement="right"
-                           title="{{ trans('webed-core::messages.are_you_sure') }}"
-                           class="btn btn-warning">
-                            {{ trans('webed-caching::base.commands.refresh_compiled_views') }}
-                        </a>
-                    </div>
-                    <div class="form-group">
-                        <a href="{{ route('admin::webed-caching.create-config-cache.get') }}"
-                           data-toggle="confirmation"
-                           data-placement="right"
-                           title="{{ trans('webed-core::messages.are_you_sure') }}"
-                           class="btn green">
-                            {{ trans('webed-caching::base.commands.create_config_cache') }}
-                        </a>
-                        <a href="{{ route('admin::webed-caching.clear-config-cache.get') }}"
-                           data-toggle="confirmation"
-                           data-placement="right"
-                           title="{{ trans('webed-core::messages.are_you_sure') }}"
-                           class="btn green-meadow">
-                            {{ trans('webed-caching::base.commands.clear_config_cache') }}
-                        </a>
-                    </div>
-                    <div class="form-group">
-                        <a href="{{ route('admin::webed-caching.optimize-class.get') }}"
-                           data-toggle="confirmation"
-                           data-placement="right"
-                           title="{{ trans('webed-core::messages.are_you_sure') }}"
-                           class="btn purple">
-                            {{ trans('webed-caching::base.commands.optimize_class_loader') }}
-                        </a>
-                        <a href="{{ route('admin::webed-caching.clear-compiled-class.get') }}"
-                           data-toggle="confirmation"
-                           data-placement="right"
-                           title="{{ trans('webed-core::messages.are_you_sure') }}"
-                           class="btn red-haze">
-                            {{ trans('webed-caching::base.commands.clear_optimized_class_loader') }}
-                        </a>
-                    </div>
-                    <div class="form-group hidden">
-                        <a href="{{ route('admin::webed-caching.create-route-cache.get') }}"
-                           data-toggle="confirmation"
-                           data-placement="right"
-                           title="{{ trans('webed-core::messages.are_you_sure') }}"
-                           class="btn yellow-crusta">
-                            {{ trans('webed-caching::base.commands.create_route_cache') }}
-                        </a>
-                        <a href="{{ route('admin::webed-caching.clear-route-cache.get') }}"
-                           data-toggle="confirmation"
-                           data-placement="right"
-                           title="{{ trans('webed-core::messages.are_you_sure') }}"
-                           class="btn purple">
-                            {{ trans('webed-caching::base.commands.clear_route_cache') }}
-                        </a>
-                    </div>
+                    <table class="table table-bordered vertical-middle table-hover">
+                        <colgroup>
+                            <col width="70%">
+                            <col width="30%">
+                        </colgroup>
+                        <tbody>
+                        <tr>
+                            <td>
+                                {{ trans('webed-caching::base.commands.clear_cms_cache.description') }}
+                            </td>
+                            <td>
+                                <a href="{{ route('admin::webed-caching.clear-cms-cache.get') }}"
+                                   data-toggle="confirmation"
+                                   data-placement="right"
+                                   title="{{ trans('webed-core::messages.are_you_sure') }}"
+                                   class="btn btn-danger btn-block">
+                                    {{ trans('webed-caching::base.commands.clear_cms_cache.title') }}
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ trans('webed-caching::base.commands.refresh_compiled_views.description') }}
+                            </td>
+                            <td>
+                                <a href="{{ route('admin::webed-caching.refresh-compiled-views.get') }}"
+                                   data-toggle="confirmation"
+                                   data-placement="right"
+                                   title="{{ trans('webed-core::messages.are_you_sure') }}"
+                                   class="btn btn-warning btn-block">
+                                    {{ trans('webed-caching::base.commands.refresh_compiled_views.title') }}
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ trans('webed-caching::base.commands.create_config_cache.description') }}
+                            </td>
+                            <td>
+                                <a href="{{ route('admin::webed-caching.create-config-cache.get') }}"
+                                   data-toggle="confirmation"
+                                   data-placement="right"
+                                   title="{{ trans('webed-core::messages.are_you_sure') }}"
+                                   class="btn green btn-block">
+                                    {{ trans('webed-caching::base.commands.create_config_cache.title') }}
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ trans('webed-caching::base.commands.clear_config_cache.description') }}
+                            </td>
+                            <td>
+                                <a href="{{ route('admin::webed-caching.clear-config-cache.get') }}"
+                                   data-toggle="confirmation"
+                                   data-placement="right"
+                                   title="{{ trans('webed-core::messages.are_you_sure') }}"
+                                   class="btn green-meadow btn-block">
+                                    {{ trans('webed-caching::base.commands.clear_config_cache.title') }}
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ trans('webed-caching::base.commands.optimize_class_loader.description') }}
+                            </td>
+                            <td>
+                                <a href="{{ route('admin::webed-caching.optimize-class.get') }}"
+                                   data-toggle="confirmation"
+                                   data-placement="right"
+                                   title="{{ trans('webed-core::messages.are_you_sure') }}"
+                                   class="btn purple btn-block">
+                                    {{ trans('webed-caching::base.commands.optimize_class_loader.title') }}
+                                </a>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                {{ trans('webed-caching::base.commands.clear_optimized_class_loader.description') }}
+                            </td>
+                            <td>
+                                <a href="{{ route('admin::webed-caching.clear-compiled-class.get') }}"
+                                   data-toggle="confirmation"
+                                   data-placement="right"
+                                   title="{{ trans('webed-core::messages.are_you_sure') }}"
+                                   class="btn red-haze btn-block">
+                                    {{ trans('webed-caching::base.commands.clear_optimized_class_loader.title') }}
+                                </a>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
             @php do_action(BASE_ACTION_META_BOXES, 'main', WEBED_CACHING . '.index', null) @endphp
